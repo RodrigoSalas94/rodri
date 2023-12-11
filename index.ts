80cbebc9795f0f97c228510baddb54cb247822c3
rodrigoacdc <rodrigodsalas94@gmail.com>
2023-12-10 22:40:45 -0300

    a

---
+++ index.ts
@@ -0,0 +1,14 @@
+// Definición de una función que calcula el área de un círculo
+function areaCirculo(radio: number) {
+    return Math.PI * radio * radio;
+  }
+  
+  // Definición de una constante que representa el radio del círculo
+  const radio = 5;
+  
+  // Llamada a la función para calcular el área
+  const area = areaCirculo(radio);
+  
+  // Impresión del área
+  console.log(area); // 78.53981633974483
+  
